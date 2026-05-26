scitex-ai/scitex-python

tests: add subprocess-coverage wiring to conftest
Per scitex-dev skill 05_development_06_subprocess-coverage.md, add the
canonical wiring (COVERAGE_PROCESS_START force-set + .pth shim) so that
child Python interpreters spawned by tests count toward coverage. Brings
this leaf in line with the 55 other leaves that already had it.

tests/conftest.py
@@ -1,3 +1,41 @@
+"""Module-import-time coverage wiring (parallel + subprocess support).
+
+`os.environ.setdefault` would be a no-op here because pytest-cov has
+already set ``COVERAGE_FILE`` to a tmp dir by the time conftest is loaded.
+
+See ``scitex_dev._skills.general.05_development_06_subprocess-coverage``.
+"""
+from __future__ import annotations
+
+import os
+import sysconfig
+from pathlib import Path
+
+_PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
+os.environ["COVERAGE_PROCESS_START"] = str(_PROJECT_ROOT / "pyproject.toml")
+os.environ["COVERAGE_FILE"] = str(_PROJECT_ROOT / ".coverage")
+
+
+def _ensure_subprocess_coverage_shim() -> None:
+    purelib = Path(sysconfig.get_paths()["purelib"])
+    pth = purelib / "_scitex_subprocess_coverage.pth"
+    shim = (
+        "import os, coverage\n"
+        "if os.environ.get('COVERAGE_PROCESS_START'):\n"
+        "    coverage.process_startup()\n"
+    )
+    try:
+        if not pth.exists() or pth.read_text() != shim:
+            pth.write_text(shim)
+    except OSError:
+        pass
+
+
+_ensure_subprocess_coverage_shim()
+
+
+# ===== existing conftest content below =====
 #!/usr/bin/env python3
 # Timestamp: "2025-07-14 16:55:30 (ywatanabe)"
 # File: tests/conftest.py

ywatanabe1989/scitex-os

tests/conftest.py
@@ -1,3 +1,41 @@
+"""Module-import-time coverage wiring (parallel + subprocess support).
+
+`os.environ.setdefault` would be a no-op here because pytest-cov has
+already set ``COVERAGE_FILE`` to a tmp dir by the time conftest is loaded.
+
+See ``scitex_dev._skills.general.05_development_06_subprocess-coverage``.
+"""
+from __future__ import annotations
+
+import os
+import sysconfig
+from pathlib import Path
+
+_PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
+os.environ["COVERAGE_PROCESS_START"] = str(_PROJECT_ROOT / "pyproject.toml")
+os.environ["COVERAGE_FILE"] = str(_PROJECT_ROOT / ".coverage")
+
+
+def _ensure_subprocess_coverage_shim() -> None:
+    purelib = Path(sysconfig.get_paths()["purelib"])
+    pth = purelib / "_scitex_os_subprocess_coverage.pth"
+    shim = (
+        "import os, coverage\n"
+        "if os.environ.get('COVERAGE_PROCESS_START'):\n"
+        "    coverage.process_startup()\n"
+    )
+    try:
+        if not pth.exists() or pth.read_text() != shim:
+            pth.write_text(shim)
+    except OSError:
+        pass
+
+
+_ensure_subprocess_coverage_shim()
+
+
+# ===== existing conftest content below =====
 """Pytest fixtures and rootdir marker for this package.
 
 An empty conftest.py at tests/ is the canonical SciTeX